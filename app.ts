8e819cc095f3912e2bcb0c44861eb91737099526
Erfan Balaghi <balaghi1999@gmail.com>
2024-12-23 22:10:55 +0330

    New Version

--- app.ts
+++ app.ts
@@ -1,94 +1,59 @@
 const formElement = document.querySelector("form") as HTMLFormElement;
 const formSubmitBtn = document.querySelector("button") as HTMLButtonElement;
-const allInputs = document.querySelectorAll<HTMLInputElement>("input");
-const [firstName, email, password, repPassword, male, female] = allInputs;
-
-type Objs = {
-  [key: string]: {
-    [key: string]: string | number | boolean | HTMLInputElement | object;
-    element: HTMLInputElement;
-    value: string;
-    id: string;
-    validate: { [key: string]: boolean };
-  };
-};
-
-function getArrts(inputProps: HTMLInputElement[]): object {
-  const obj: Objs = {};
-
-  inputProps.forEach((item) => {
-    const id: string = item.id;
-    const getAttr = item.attributes;
-    const setAttr: {
-      [key: string]: string | number | boolean | object;
-      id: string;
-      validate: { [key: string]: boolean };
-    } = {
-      id: "",
-      validate: {},
-    };
-
-    for (const key of getAttr) {
-      if (
-        key.name === "minlength" ||
-        key.name === "maxlength" ||
-        key.name === "required"
-      ) {
-        setAttr[key.name] = +key.value || key.value;
-        setAttr.validate = { ...setAttr.validate, [key.name]: false };
-        setAttr.id = id;
-      }
-    }
-
-    obj[id] = {
-      ...setAttr,
-      value: item.value,
-      element: item,
-      validate: { ...setAttr.validate },
-    };
-  });
-
-  return obj;
-}
+const [firstName, email, password, repPassword]: HTMLFormControlsCollection =
+  formElement.elements;
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
 });
 
+function getAttrs(
+  elements = [firstName, email, password, repPassword]
+): object {
+  const getAttrs: {
+    [key: string]: object;
+  } = {};
+  for (const key of elements) {
+    const attrKeys: {
+      [key: string]: object;
+    } = {};
+    if ("required" in key.attributes) {
+      attrKeys.required = {
+        value: true,
+        errorMessage: key.getAttribute("data-requirederror"),
+      };
+    }
+    if ("minlength" in key.attributes) {
+      const getminlength = key.getAttribute("minlength");
+      attrKeys.minlength = {
+        value: typeof getminlength === "string" ? +getminlength : 0,
+        errorMessage: key.getAttribute("data-minlengtherror"),
+      };
+    }
+    if ("maxlength" in key.attributes) {
+      const getmaxlength = key.getAttribute("maxlength");
+      attrKeys.maxlength = {
+        value: typeof getmaxlength === "string" ? +getmaxlength : 0,
+        errorMessage: key.getAttribute("data-maxlengtherror"),
+      };
+    }
+    getAttrs[key.id] = attrKeys;
+  }
+  return getAttrs;
+}
+
+function checkFileds(
+  attrFields: object,
+  elements = [firstName, email, password, repPassword]
+) {
+  for (const key of elements) {
+    console.log(key);
+  }
+}
+
 formSubmitBtn.addEventListener("mouseup", (event) => {
   event.preventDefault();
-
-  const data: Objs = { ...getArrts([firstName, email, password, repPassword]) };
-  for (const key in data) {
-    if (
-      data[key]["minlength"] &&
-      +data[key].value.length >= +data[key]["minlength"]
-    ) {
-      data[key].validate.minlength = true;
-    }
-    if (data[key]["required"] !== undefined && data[key].value !== "") {
-      data[key].validate.required = true;
-    }
-  }
-
-  const valdates = Object.values(data);
-  const errors: object[] = valdates.map((item) => {
-    const list: object[] = [];
-    const validateKeys: string[] = Object.keys(item.validate);
-    const id: string = item.id;
-    const attrs: NamedNodeMap = item.element.attributes;
-    for (let key of validateKeys) {
-      if (!item.validate[key]) {
-        const errorMassage: string | null = item.element.getAttribute(
-          `data-${key}error`
-        );
-        console.log(typeof errorMassage);
-        list.push({ value: `data-${key}error`, message: errorMassage });
-        // list.push(`data-${key}error`);
-      }
-    }
-    return { [id]: {...list} };
-  });
-
-  console.log(errors);
+  const attrValues: object = getAttrs();
+  checkFileds(attrValues);
+  // console.log(attrValues);
 });
---
+++ oldVersion.ts
@@ -0,0 +1,92 @@
+const formElement = document.querySelector("form") as HTMLFormElement;
+const formSubmitBtn = document.querySelector("button") as HTMLButtonElement;
+const allInputs = document.querySelectorAll<HTMLInputElement>("input");
+const [firstName, email, password, repPassword, male, female] = allInputs;
+
+type Objs = {
+  [key: string]: {
+    [key: string]: string | number | boolean | HTMLInputElement | object;
+    element: HTMLInputElement;
+    value: string;
+    id: string;
+    validate: { [key: string]: boolean };
+  };
+};
+
+function getArrts(inputProps: HTMLInputElement[]): object {
+  const obj: Objs = {};
+
+  inputProps.forEach((item) => {
+    const id: string = item.id;
+    const getAttr = item.attributes;
+    const setAttr: {
+      [key: string]: string | number | boolean | object;
+      id: string;
+      validate: { [key: string]: boolean };
+    } = {
+      id: "",
+      validate: {},
+    };
+
+    for (const key of getAttr) {
+      if (
+        key.name === "minlength" ||
+        key.name === "maxlength" ||
+        key.name === "required"
+      ) {
+        setAttr[key.name] = +key.value || key.value;
+        setAttr.validate = { ...setAttr.validate, [key.name]: false };
+        setAttr.id = id;
+      }
+    }
+
+    obj[id] = {
+      ...setAttr,
+      value: item.value,
+      element: item,
+      validate: { ...setAttr.validate },
+    };
+  });
+
+  return obj;
+}
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+formSubmitBtn.addEventListener("mouseup", (event) => {
+  event.preventDefault();
+  const data: Objs = { ...getArrts([firstName, email, password, repPassword]) };
+  for (const key in data) {
+    if (data[key]["required"] !== undefined && data[key].value !== "") {
+      data[key].validate.required = true;
+    } else if (
+      data[key]["minlength"] &&
+      +data[key].value.length >= +data[key]["minlength"]
+    ) {
+      data[key].validate.minlength = true;
+    }
+  }
+  const valdates = Object.values(data);
+  const errors: object[] = valdates.map((item) => {
+    console.log(item);
+    const list: object[] = [];
+    const validateKeys: string[] = Object.keys(item.validate);
+    const id: string = item.id;
+    const attrs: NamedNodeMap = item.element.attributes;
+    for (let key of validateKeys) {
+      if (!item.validate[key]) {
+        const errorMassage: string | null = item.element.getAttribute(
+          `data-${key}error`
+        );
+        console.log(item.element);
+        list.push({ value: key, message: errorMassage });
+        break;
+      }
+    }
+    return { [id]: list };
+  });
+
+  console.log(errors);
+});
